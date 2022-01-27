cb54b90584dfa3a96412bf9d0c9504d52feb54f9
Brian Duong <brianminhduong@gmail.com>
2022-01-27 16:34:42 +0000

    Add function to retrieve reservation data for user

--- 1_queries/6_test.sql
+++
@@ -1,8 +0,0 @@
-SELECT reservations.id, properties.title, properties.number_of_bedrooms, properties.number_of_bathrooms, properties.parking_spaces, properties.cost_per_night, reservations.start_date, avg(rating) as average_rating
-  FROM reservations
-  JOIN properties ON reservations.property_id = properties.id
-  JOIN property_reviews ON properties.id = property_reviews.property_id
-  WHERE reservations.guest_id = 4
-  GROUP BY properties.id, reservations.id
-  ORDER BY reservations.start_date
-  LIMIT 10;
